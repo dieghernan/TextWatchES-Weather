a8a39f5a37919da848da6cbd4be7c838e9524886
dieghernan <diego.hernangomezherrero@gmail.com>
2017-04-25 17:01:21 +0200

    Update num2words-fr.h

--- src/c/num2words-fr.h
+++ src/c/num2words-fr.h
@@ -1,13 +1,9 @@
 #pragma once
 #include "string.h"
-
-
 void time_to_3words_FR(int hours, int minutes,int *LineBold, char *line1, char *line2, char *line3);
 void PopatInit_FR(int minute, int *lenatinit);
 void WriteDate_FR(int WD, int Mnth, int Dy, char *iterweekday, char *iterdate, char *itermonth );
 void Animations_FR(int Minute, int *LenBefore, int *LenNow, int *LenAfter);
-
 extern const char* const MONTHS_FR[];
 extern const char* const DAYS_FR[];
 extern const char* const WEEKDAY_FR[];
-extern const char* const MIN_FR2[];
