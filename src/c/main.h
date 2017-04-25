4a8907cd94ec170c036901465f0325daaecddfa0
dieghernan <diego.hernangomezherrero@gmail.com>
2017-04-25 23:20:31 +0200

    Fix

--- src/c/main.h
+++ src/c/main.h
@@ -1,15 +1,12 @@
 #pragma once
 #include <pebble.h>
-
 #define SETTINGS_KEY 1
-
 // A structure containing our settings
 typedef struct ClaySettings {
   GColor BackgroundColor;
   GColor ForegroundColor;
   GColor BackgroundColorNight;
   GColor ForegroundColorNight;
-  
   bool NightTheme;
   bool WeatherUnit;
   int LangKey;
@@ -21,16 +18,4 @@ typedef struct ClaySettings {
   int HourSunrise;
   int HourSunset;
   int UpSlider;
-    
-    
 } __attribute__((__packed__)) ClaySettings;
-
-static void prv_default_settings();
-static void prv_load_settings();
-static void prv_save_settings();
-static void prv_inbox_received_handler(DictionaryIterator *iter, void *context);
-static void prv_window_load(Window *window);
-static void prv_window_unload(Window *window);
-static void prv_init(void);
-static void prv_deinit(void);
-
--- src/c/num2words-br.h
+++ src/c/num2words-br.h
@@ -4,6 +4,3 @@ void time_to_3words_BR(int hours, int minutes,int *LineBold, char *line1, char *
 void PopatInit_BR(int minute, int *lenatinit);
 void WriteDate_BR(int WD, int Mnth, int Dy, char *iterweekday, char *iterdate, char *itermonth );
 void Animations_BR(int Minute, int *LenBefore, int *LenNow, int *LenAfter);
-extern const char* const MONTHS_BR[];
-extern const char* const DAYS_BR[];
-extern const char* const WEEKDAY_BR[];
