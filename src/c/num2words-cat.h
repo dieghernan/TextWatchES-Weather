291763e4d749bf478ecd277b86f09f9f14c72199
dieghernan <diego.hernangomezherrero@gmail.com>
2017-05-03 12:53:44 +0200

    Update num2words-cat.h

--- src/c/num2words-cat.h
+++ src/c/num2words-cat.h
@@ -1,1 +1,4 @@
-
+#pragma once
+#include "string.h"
+void time_to_3words_CAT(int hours, int minutes,int *LineBold, char *line1, char *line2, char *line3);
+void WriteDate_CAT(int WD, int Mnth, int Dy, char *iterweekday, char *iterdate, char *itermonth );
